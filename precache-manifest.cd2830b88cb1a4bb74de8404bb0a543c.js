0a4e56e47c6cdf24916faf100ffcba54c781a4fb
Your Name <you@example.com>
2020-04-08 13:03:23 +0700

    123

---
+++ precache-manifest.cd2830b88cb1a4bb74de8404bb0a543c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "328ee8d83d34c8c809b15b692459bb0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ad40705a95852fd2e50",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "45c9b1ab90cf2d96895b",
+    "url": "/static/css/main.c8f0f3a6.chunk.css"
+  },
+  {
+    "revision": "1ad40705a95852fd2e50",
+    "url": "/static/js/2.f2a54cac.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.f2a54cac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45c9b1ab90cf2d96895b",
+    "url": "/static/js/main.91ae0ec5.chunk.js"
+  },
+  {
+    "revision": "a572f1fc606f246f3211",
+    "url": "/static/js/runtime-main.4b0f4d02.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
